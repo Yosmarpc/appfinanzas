2c8ae2fcc1f277af5cf5268ed2797692440e7f65
Yosmar Hinestroza <consultor_yhinestroza@vitamina.cl>
2022-02-09 18:38:06 -0300

    feat: se realiza cambio de en diferente parte de los modulos se agrega loading para cada trozo de los componente

--- src/App.js
+++ src/App.js
@@ -9,41 +9,45 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import FormCalculadora from "./components/calculadora/FormCalculadora";
 import React, { useEffect } from "react";
 import Graficos from "./components/Graficos/Graficos";
+import PrincipalCalculadora from "./components/calculadora/PrincipalCalculadora";
+import { Spinner } from "react-bootstrap";
 function App() {
   const [loadings, setLoadings] = React.useState(false);
   const url = "https://mindicador.cl/api";
   const [dataIndicadorcal, setDataIndcalc] = React.useState();
 
-
   useEffect(() => {
-      setLoadings(true);
+    setLoadings(true);
     fetch(`${url}`)
-    .then((response) => response.json())
-    .then(data =>setDataIndcalc(data))
-    .catch((error) => console.error(error))
-   setTimeout(()=>{setLoadings(false)},2000)
-  },[])
+      .then((response) => response.json())
+      .then((data) => setDataIndcalc(data))
+      .catch((error) => console.error(error));
+    setTimeout(() => {
+      setLoadings(false);
+    }, 2000);
+  }, []);
 
   return (
     <>
-    <div variant="dark">
-       <NavbarMenu />
-    </div>
-
-    <div variant="dark">
-    {loadings ? <div className="text-center my-4 text-dark">Loading espere...</div> :
-      <div className="container-md">
-      <FormCalculadora dataCal={dataIndicadorcal} />
-      
-      <Index />
-      <div className="col-lg-12">
-      <Graficos />
+      <div variant="dark">
+        <NavbarMenu />
       </div>
- 
-      </div>
-    }
-    </div>
- 
+      {loadings ? (
+        <div className="text-center my-5 text-dark" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}><Spinner animation="grow" variant="secondary" /></div>
+      ) : (
+        <div>
+          <PrincipalCalculadora dataCal={dataIndicadorcal} />
+          {/* <FormCalculadora dataCal={dataIndicadorcal} /> */}
+          <div variant="dark">
+            <div className="container-md">
+              <Index />
+              <div className="col-lg-12">
+                <Graficos />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
